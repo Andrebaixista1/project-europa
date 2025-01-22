22933cea7cbc5e0fa4e62935601e84939c1de048
André Felipe Pinto <68709277+Andrebaixista1@users.noreply.github.com>
2025-01-22 11:52:15 -0300

    token gerado automatico

--- src/ConsultaINSS.js
+++ src/ConsultaINSS.js
@@ -1,8 +1,29 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+
+const API_URL = "https://api.ajin.io/v3/auth/sign-in";
+const credentials = {
+    accessId: "jacqueline.vieira@qualiconsig.com.br",
+    password: "Jacque@324",
+    authKey: "",
+    type: "",
+    stayConnected: true
+};
+
+const getAuthToken = async () => {
+    try {
+        const response = await axios.post(API_URL, credentials, {
+            headers: { "Content-Type": "application/json" }
+        });
+        return response.data.token;
+    } catch (error) {
+        console.error("❌ Erro ao obter o token:", error.message);
+        return null;
+    }
+};
 
 const formatDate = (dateStr) => {
     if (!dateStr) return "-";
@@ -22,6 +43,15 @@ const ConsultaINSS = () => {
     const [nb, setNb] = useState("");
     const [dados, setDados] = useState(null);
     const [loading, setLoading] = useState(false);
+    const [token, setToken] = useState("");
+
+    useEffect(() => {
+        const fetchToken = async () => {
+            const newToken = await getAuthToken();
+            setToken(newToken);
+        };
+        fetchToken();
+    }, []);
 
     const handleSearch = async () => {
         if (!cpf || !nb) {
@@ -29,16 +59,21 @@ const ConsultaINSS = () => {
             return;
         }
 
+        if (!token) {
+            toast.error("Erro ao obter o token de autenticação!");
+            return;
+        }
+
         setLoading(true);
         
         const URL = "https://api.ajin.io/v3/query-inss-balances/finder/await";
         const HEADERS = {
-            Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiYjg4MzMxMjUtZWIyNi00MjVjLTg3NzAtM2QwNGFiMWM4ZWJlIiwic2Vzc2lvbl9pZCI6ImZhN2I1MjQ5LTMxMzktNDgwNi05M2YxLTQ3MDE0MTBkOGNhYiIsImhvc3QiOiJhcGkuYWppbi5pbyIsImlwIjoiMjAxLjAuMjEuMTQzIiwibmJmIjoxNzM3NTU0MjcyLCJleHAiOjE3Mzc2NDA2NzIsImlhdCI6MTczNzU1NDI3Mn0.wHUooA6WhOQ0QrlA6k7sqqlkrtDwdwT91GQc-VKUHg4",
+            Authorization: `Bearer ${token}`,
             "Content-Type": "application/json"
         };
 
         try {
-            const response = await axios.post(URL, { identity: cpf, benefitNumber: nb, attempts: 10  }, { headers: HEADERS });
+            const response = await axios.post(URL, { identity: cpf, benefitNumber: nb, attempts: 10 }, { headers: HEADERS });
             setDados(response.data);
             toast.success(`Consulta realizada com sucesso! Status: ${response.status}`);
         } catch (error) {
@@ -49,54 +84,9 @@ const ConsultaINSS = () => {
         setLoading(false);
     };
 
-    const handleCopyTable = () => {
-        if (!dados) return;
-        
-        let tableText = "Consulta IN100 - Projeto Europa v0.1.2025\n\n";
-        Object.entries({
-            "Número do Benefício": dados.benefitNumber,
-            "Número do Documento": dados.documentNumber,
-            "Nome": dados.name,
-            "Estado": dados.state,
-            "Pensão": dados.alimony === "payer" ? "SIM" : "NÃO",
-            "Data de Nascimento": formatDate(dados.birthDate),
-            "Tipo de Bloqueio": dados.blockType === "not_blocked" ? "Nenhum" : dados.blockType,
-            "Data de Concessão": formatDate(dados.grantDate),
-            "Tipo de Crédito": dados.creditType,
-            "Limite Cartão Benefício": dados.benefitCardLimit?.toLocaleString("pt-BR", { style: "currency", currency: "BRL" }),
-            "Saldo Cartão Benefício": dados.benefitCardBalance?.toLocaleString("pt-BR", { style: "currency", currency: "BRL" }),
-            "Status do Benefício": dados.benefitStatus === "elegible" ? "Elegível" : dados.benefitStatus,
-            "Data Fim Benefício": formatDate(dados.benefitEndDate),
-            "Limite Cartão Consignado": dados.consignedCardLimit?.toLocaleString("pt-BR", { style: "currency", currency: "BRL" }),
-            "Saldo Cartão Consignado": dados.consignedCardBalance?.toLocaleString("pt-BR", { style: "currency", currency: "BRL" }),
-            "Saldo Crédito Consignado": dados.consignedCreditBalance?.toLocaleString("pt-BR", { style: "currency", currency: "BRL" }),
-            "Saldo Total Máximo": dados.maxTotalBalance?.toLocaleString("pt-BR", { style: "currency", currency: "BRL" }),
-            "Saldo Total Utilizado": dados.usedTotalBalance?.toLocaleString("pt-BR", { style: "currency", currency: "BRL" }),
-            "Saldo Total Disponível": dados.availableTotalBalance?.toLocaleString("pt-BR", { style: "currency", currency: "BRL" }),
-            "Data da Consulta": formatDateTime(dados.queryDate),
-            "Data de Retorno da Consulta": formatDateTime(dados.queryReturnDate),
-            "Tempo de Retorno da Consulta": dados.queryReturnTime,
-        }).forEach(([key, value]) => {
-            tableText += `${key}: ${value}\n`;
-        });
-
-        navigator.clipboard.writeText(tableText)
-            .then(() => toast.success("Tabela copiada para a área de transferência!"))
-            .catch(() => toast.error("Erro ao copiar a tabela."));
-    };
-
     return (
-        <div className="container mt-5 position-relative">
-            {loading && (
-                <div className="loading-overlay position-fixed top-0 start-0 w-100 h-100 bg-dark bg-opacity-75 d-flex justify-content-center align-items-center">
-                    <div className="spinner-border text-light" role="status">
-                        <span className="visually-hidden">Carregando...</span>
-                    </div>
-                </div>
-            )}
-            
-            <h2 className="mb-4">Consulta IN100 - Projeto Europa v0.1.2025</h2>
-
+        <div className="container mt-5">
+            <h2>Consulta IN100 - Projeto Europa v0.1.2025</h2>
             <div className="row mb-3">
                 <div className="col-md-4">
                     <label className="form-label">CPF:</label>
@@ -116,37 +106,33 @@ const ConsultaINSS = () => {
                     <table className="table table-bordered">
                         <tbody>
                         {Object.entries({
-                               "Número do Benefício": dados.benefitNumber,
-            "Número do Documento": dados.documentNumber,
-            "Nome": dados.name,
-            "Estado": dados.state,
-            "Pensão": dados.alimony === "payer" ? "SIM" : "NÃO",
-            "Data de Nascimento": formatDate(dados.birthDate),
-            "Tipo de Bloqueio": dados.blockType === "not_blocked" ? "Nenhum" : dados.blockType,
-            "Data de Concessão": formatDate(dados.grantDate),
-            "Tipo de Crédito": dados.creditType,
-            "Limite Cartão Benefício": dados.benefitCardLimit?.toLocaleString("pt-BR", { style: "currency", currency: "BRL" }),
-            "Saldo Cartão Benefício": dados.benefitCardBalance?.toLocaleString("pt-BR", { style: "currency", currency: "BRL" }),
-            "Status do Benefício": dados.benefitStatus === "elegible" ? "Elegível" : dados.benefitStatus,
-            "Data Fim Benefício": formatDate(dados.benefitEndDate),
-            "Limite Cartão Consignado": dados.consignedCardLimit?.toLocaleString("pt-BR", { style: "currency", currency: "BRL" }),
-            "Saldo Cartão Consignado": dados.consignedCardBalance?.toLocaleString("pt-BR", { style: "currency", currency: "BRL" }),
-            "Saldo Crédito Consignado": dados.consignedCreditBalance?.toLocaleString("pt-BR", { style: "currency", currency: "BRL" }),
-            "Saldo Total Máximo": dados.maxTotalBalance?.toLocaleString("pt-BR", { style: "currency", currency: "BRL" }),
-            "Saldo Total Utilizado": dados.usedTotalBalance?.toLocaleString("pt-BR", { style: "currency", currency: "BRL" }),
-            "Saldo Total Disponível": dados.availableTotalBalance?.toLocaleString("pt-BR", { style: "currency", currency: "BRL" }),
-            "Data da Consulta": formatDateTime(dados.queryDate),
-            "Data de Retorno da Consulta": formatDateTime(dados.queryReturnDate),
-            "Tempo de Retorno da Consulta": dados.queryReturnTime,
-                            }).map(([key, value]) => (
-                                <tr key={key}>
-                                    <th>{key}</th>
-                                    <td>{value}</td>
-                                </tr>
-                            ))}
+                            "Número do Benefício": dados.benefitNumber,
+                            "Número do Documento": dados.documentNumber,
+                            "Nome": dados.name,
+                            "Estado": dados.state,
+                            "Pensão": dados.alimony === "payer" ? "SIM" : "NÃO",
+                            "Data de Nascimento": formatDate(dados.birthDate),
+                            "Tipo de Bloqueio": dados.blockType === "not_blocked" ? "Nenhum" : dados.blockType,
+                            "Data de Concessão": formatDate(dados.grantDate),
+                            "Tipo de Crédito": dados.creditType,
+                            "Limite Cartão Benefício": dados.benefitCardLimit?.toLocaleString("pt-BR", { style: "currency", currency: "BRL" }),
+                            "Saldo Cartão Benefício": dados.benefitCardBalance?.toLocaleString("pt-BR", { style: "currency", currency: "BRL" }),
+                            "Status do Benefício": dados.benefitStatus === "elegible" ? "Elegível" : dados.benefitStatus,
+                            "Data Fim Benefício": formatDate(dados.benefitEndDate),
+                            "Limite Cartão Consignado": dados.consignedCardLimit?.toLocaleString("pt-BR", { style: "currency", currency: "BRL" }),
+                            "Saldo Cartão Consignado": dados.consignedCardBalance?.toLocaleString("pt-BR", { style: "currency", currency: "BRL" }),
+                            "Saldo Crédito Consignado": dados.consignedCreditBalance?.toLocaleString("pt-BR", { style: "currency", currency: "BRL" }),
+                            "Saldo Total Máximo": dados.maxTotalBalance?.toLocaleString("pt-BR", { style: "currency", currency: "BRL" }),
+                            "Saldo Total Utilizado": dados.usedTotalBalance?.toLocaleString("pt-BR", { style: "currency", currency: "BRL" }),
+                            "Saldo Total Disponível": dados.availableTotalBalance?.toLocaleString("pt-BR", { style: "currency", currency: "BRL" })
+                        }).map(([key, value]) => (
+                            <tr key={key}>
+                                <th>{key}</th>
+                                <td>{value}</td>
+                            </tr>
+                        ))}
                         </tbody>
                     </table>
-                    <button className="btn btn-secondary mt-3" onClick={handleCopyTable}>Copiar Tabela</button>
                 </div>
             )}
         </div>
